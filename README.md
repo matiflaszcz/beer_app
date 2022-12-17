876153d1b3e06979fdf9b283375b9bc19acc8a9d
Mateusz Flaszczyński <mateuszflaszczynski@gmail.com>
2022-12-17 12:22:28 +0100

    add setup and usage in readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # beer_app
+
+## setup
+
+### 1 Install python3
+
+```
+install python3 
+```
+
+### 2 Install module argparse and requests
+
+
+## usage
+
+insert under line in terminal
+
+```bash
+python3 beer.py beef
+```
+
+result
+
+```
+Beers preffered for your dish are:
+
+Libertine Porter
+AB:06
+
+ Bon appetit!
+```
--- beer.py
+++ beer.py
@@ -1,14 +1,11 @@
 
-import webbrowser
 import requests
 import argparse
-
-# webbrowser.open('https://api.punkapi.com/v2/beers')
 
 req = requests.get('https://api.punkapi.com/v2/beers')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("danie",type=str,help="Give me the name of the food ")
+parser.add_argument("dish",type=str,help="Give me the name of the food ")
 args = parser.parse_args()
 
 print("Beers preffered for your dish are:\n")
@@ -16,10 +13,10 @@ print("Beers preffered for your dish are:\n")
 find_beers=0
 for  element in range(24):
     join_food=" ".join(req.json()[element]["food_pairing"])
-    if join_food.find(args.danie)>=0:
+    if join_food.find(args.dish)>=0:
        find_beers+=1
        print(req.json()[element]["name"])
     elif element==23 and find_beers==0:
-       print("I don't have this food on my list, try anther")    
+       print("I don't have this food on my list, try anther dish")    
 
 print("\n Bon appetit!")
