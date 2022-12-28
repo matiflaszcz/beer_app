b975a2325701a8e4a6b78b0f08f4425c51a65980
student <student@henson.com>
2022-12-28 22:11:41 +0100

    add first test version

--- beer.py
+++ beer.py
@@ -4,19 +4,33 @@ import argparse
 
 req = requests.get('https://api.punkapi.com/v2/beers')
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dish",type=str,help="Give me the name of the food ")
-args = parser.parse_args()
+def main(args):
+   print("Beers preffered for your dish are:\n")
 
-print("Beers preffered for your dish are:\n")
+   beers = get_beers(args.dish)
 
-find_beers=0
-for  element in range(24):
-    join_food=" ".join(req.json()[element]["food_pairing"])
-    if join_food.find(args.dish)>=0:
-       find_beers+=1
-       print(req.json()[element]["name"])
-    elif element==23 and find_beers==0:
-       print("I don't have this food on my list, try anther dish")    
+   for beer in beers:
+      print(beer)
 
-print("\n Bon appetit!")
+   print("\n Bon appetit!")
+
+def get_beers(food):
+   find_beers=0
+   beers = []
+   
+   for  element in range(24):
+      join_food=" ".join(req.json()[element]["food_pairing"])
+      if join_food.find(food)>=0:
+         # print(join_food)
+         find_beers+=1
+         beers.append(req.json()[element]["name"])
+      elif element==23 and find_beers==0:
+         beers = []
+   return beers 
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument("dish",type=str, help="Give me the name of the food ")
+   args = parser.parse_args()
+
+   main(args)
